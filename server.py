9f756fd0b328e490c295fdf718643164b9e90519
Masterauto <wandinmai@gmail.com>
2025-03-28 17:09:38 +0700

    Update server.py

--- server.py
+++ server.py
@@ -13,6 +13,7 @@ def download():
     ydl_opts = {
         'outtmpl': output_file,
         'format': 'mp4/bestaudio/best',
+        'cookiefile': 'cookies.txt',
         'http_headers': {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
         }
